dfc4300a6634abdbe3d45e9c644495e3013b4fdd
Rootdiv <rootdiv@mail.ru>
2023-02-21 17:37:47 +0300

    feat: added Header and Container

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from 'components/Header/Header';
+import { Container } from 'components/Container/Container';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <nav>
+          <Container />
+        </nav>
+        <section></section>
+      </main>
+    </>
+  );
+};
